4ecc69485c3897c5ff4c24903992dc3d1f26043a
Frank Davies <fdavies93@gmail.com>
2022-12-02 07:34:12 +0800

    Updated with method to generate HR records table

--- reference/database-example.py
+++ reference/database-example.py
@@ -1,5 +1,13 @@
 import random
 
+def make_hr_records(contacts):
+    entries = []
+    contact_sample = random.sample(contacts, round(len(contacts) / 2))
+    for contact in contact_sample:
+        # columns: ID number, isJedi, isSith, salary in USD, midichlorian count
+        entries.append([ contact[0], bool(random.getrandbits(1)), bool(random.getrandbits(1)), random.randint(16000, 250000), random.randint(0, 10000)])
+    return entries
+
 def generate_entries(to_make : int = 1000):
     first_names = ["Luke", "Leia", "Han", "Obi-Wan", "Darth", "Sheev", "Ben", "Rey", "Lando", "Jabba", "Padme", "Chewbacca", "R2", "Wedge", "JarJar", "Count", "Boba", "Jango"]
     last_names = ["Skywalker", "Organa", "Solo", "Kenobi", "Vader", "Palpatine", "Calrissian", "Von Hutt", "Amidala", "D2", "Antilles", "Bane", "Binks", "Dooku", "Fett"]
@@ -13,4 +21,7 @@ def generate_entries(to_make : int = 1000):
 
     return entries
 
-print(generate_entries(10))
+employees = generate_entries(100)
+hr_records = make_hr_records(employees)
+print(employees)
+print(hr_records)
