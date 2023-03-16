2ac8ad5c812c57d63c7828a8b66eb0d83ba29161
Frank Davies <fdavies93@gmail.com>
2023-03-17 07:53:45 +0800

    Added quick problem for BSTs

---
+++ python/problems/bst.py
@@ -0,0 +1,23 @@
+class BstNode:
+    def __init__(self, value: int):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class Bst:
+    def insert(value):
+        # Insert a node by value, depth-first search
+        pass
+
+    def remove(value):
+        # Remove a node by value, depth-first search
+        pass
+
+    def get(value) -> BstNode:
+        # Retrieve a node by value
+        pass
+
+    def print():
+        # Traverse the tree breadth-first and print out the results
+        pass
