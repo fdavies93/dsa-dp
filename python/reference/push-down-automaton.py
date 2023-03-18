92922af5f6899207c8e818592090ff5d6665bc57
Frank Davies <fdavies93@gmail.com>
2023-03-18 21:12:53 +0800

    Successfully building crude parse tree from list

--- python/reference/push-down-automaton.py
+++ python/reference/push-down-automaton.py
@@ -2,6 +2,7 @@ from enum import IntEnum
 from dataclasses import dataclass
 from collections import deque
 from copy import deepcopy
+from typing import Union
 
 class LEX_TYPES(IntEnum):
     NONE = 0
@@ -53,92 +54,12 @@ class MathLexer:
         if len(cur_token) > 0:
             token_list.append( Token(cur_token, cur_type))
         return token_list
-    
-    def second_pass(self, input : list[Token]) -> list[Token]:
-        pass
 
-    def lex_string(self, input) -> list[Token]:
-        split_numbers = self.first_pass(input)
-        print(split_numbers)
-        tokenised = self.second_pass(split_numbers)
-        print(tokenised)
-        return split_numbers
 
-# Formal grammar: all tokens must evaluate to either a single number OR two
-# numbers separated by an operation
-# A number is one of the following:
-# 1. A single number token separated by preceding and succeeding tokens by an
-# operation such as +,-,/,* (ie a real operation, not syntax)
-# 2. A number token preceded by . - this is equivalent to 0.x
-# 3. Two number tokens separated by . ; parsed into a single floating point
-# number
-# - is ambiguous; if it falls at the start of a sequence or after an operator 
-# and is followed by a number, it is part of that number (i.e. the number is 
-# negative) If it falls between two numbers then it is an operation
-# ( resets the parser state and pushes the previous state onto the stack
-# ) pops the previous state from the stack
-
-class ParseTreeNode():
-    def __init__(self, token):
-        self.token = token
-        self.left = None
-        self.right = None
-
-class State(IntEnum):
-    START = 0
-    PARSE_DOT = 1
-    PARSE_NUMBER = 2
-    PARSE_OPERATOR = 3
-    PARSE_MINUS = 4
-    COMPLETE = 5
-
-class MathParser:
-    def __init__(self):
-        self.parse_tree = None
-        self.cur_node = None
-        self.state_fns = [
-            self.state_start,
-            self.state_dot,
-            self.state_number,
-            self.state_operator,
-            self.state_minus
-        ]
-        self.index = 0
-        self.tokens = []
-        self.state = State.START
-
-    def parse(self, in_tokens: deque[Token]):
-        if len(in_tokens) == 0:
-            self.parse_tree = None
-            return
-                
-        self.state = State.START
-        self.tokens = deepcopy(in_tokens)
-        self.parse_tree = None
-        self.index = 0
-
-        self.state_number(self.tokens)
-        # while(self.state != State.COMPLETE and self.index < len(self.tokens)):
-        #     self.state_fns[self.state]()
-
-    def state_start(self):
-        cur = self.tokens[self.index]
-        if cur.type == LEX_TYPES.NUMBER:
-            self.state = State.PARSE_NUMBER
-        elif cur.type == LEX_TYPES.OPERATOR and (cur.value == "." or cur.value == "-"):
-            self.state = State.PARSE_NUMBER
-        else:
-            # first input must always be a number
-            raise ValueError()
-
-    def state_dot(self):
-        pass
-
-    def state_number(self, tokens: list[Token]):
+    def second_pass(self, tokens: list[Token]):
         # three valid inputs to this state
         # .# ; -# ; #.# ; -#.# ; #
         allowDot = True
-        allowNegative = True
         prevWasNumber = False
         aggregate_val = ""
         index = 0
@@ -162,7 +83,6 @@ class MathParser:
                         raise ValueError()
                 elif cur.value == "-" and len(aggregate_val) == 0:
                     aggregate_val += "-"
-                    allowNegative = False
                 elif cur.value == "-" and aggregate_val == ".":
                     raise ValueError()
                     # this will ignore negatives in weird places (after decimal point) but not raise an error
@@ -175,39 +95,112 @@ class MathParser:
                         tokens_out.append(Token(aggregate_val, LEX_TYPES.NUMBER))
                     tokens_out.append(cur)
                     aggregate_val = ""
-                    allowNegative = True
                     allowDot = True
-                    print(tokens_out)
             elif cur.type == LEX_TYPES.NUMBER:
                 if prevWasNumber:
                     raise ValueError()
                 aggregate_val += cur.value
                 prevWasNumber = True
             index += 1
-            # only allow negative on first character
-            # allowNegative = False
         
         # must end with number
         if aggregate_val[-1] not in {str(x) for x in range(10)}:
             raise ValueError
         
         tokens_out.append(Token(aggregate_val, LEX_TYPES.NUMBER))
-        
-        print(tokens_out)
 
-    def state_operator(self):
-        pass
+        return tokens_out
 
-    def state_minus(self):
-        pass
+    def lex_string(self, input) -> list[Token]:
+        split_numbers = self.first_pass(input)
+        print(split_numbers)
+        tokenised = self.second_pass(split_numbers)
+        # print(tokenised)
+        return tokenised
 
-    def execute(self):
-        pass
+# Formal grammar: all tokens must evaluate to either a single number OR two
+# numbers separated by an operation
+# A number is one of the following:
+# 1. A single number token separated by preceding and succeeding tokens by an
+# operation such as +,-,/,* (ie a real operation, not syntax)
+# 2. A number token preceded by . - this is equivalent to 0.x
+# 3. Two number tokens separated by . ; parsed into a single floating point
+# number
+# - is ambiguous; if it falls at the start of a sequence or after an operator 
+# and is followed by a number, it is part of that number (i.e. the number is 
+# negative) If it falls between two numbers then it is an operation
+# ( resets the parser state and pushes the previous state onto the stack
+# ) pops the previous state from the stack
+
+class ParseTreeNode():
+    def __init__(self, token):
+        self.token = token
+        self.left = None
+        self.right = None
+
+class ParseTree():
+    def __init__(self):
+        self.root = None
+    
+    def insert_at_root(self, node : ParseTreeNode):
+        if self.root is None:
+            self.root = node
+            return
 
 
+class State(IntEnum):
+    START = 0
+    PARSE_DOT = 1
+    PARSE_NUMBER = 2
+    PARSE_OPERATOR = 3
+    PARSE_MINUS = 4
+    COMPLETE = 5
+
+class MathParser:
+
+    def get_first_operator_index(self, tokens : list[Union[Token, list]], operator : str):
+        for i, token in enumerate(tokens):
+            if isinstance(token, Token) and token.value == operator:
+                return i
+        return None
+
+    def parse(self, tokens: list[Token]):
+        if len(tokens) == 0:
+            return None
+
+        root_node = None
+        operation_precedence = ['/','*','+','-']
+
+        min_i = 0
+        max_i = len(tokens)
+
+        cur_list : list[Token] = deepcopy(tokens)
+
+        for operation in operation_precedence:
+            # get the next operation of this type in the list
+            next_operation = self.get_first_operator_index(cur_list, operation)
+            while next_operation != None:
+                next_list = []
+                # combine left and right-hand elements
+                combined_elements = cur_list[next_operation-1:next_operation+2]
+                next_list = cur_list[:next_operation-1]
+                next_list.append(combined_elements)
+                next_list.extend(cur_list[next_operation+2:])
+                cur_list = next_list
+                print(f"Operator {operation} found at index {next_operation}")
+                next_operation = self.get_first_operator_index(cur_list, operation)                
+        cur_list = cur_list[0]
+        print(cur_list)
+
+        # locate opening brackets; if found, locate closing brackets. also, push to stack
+        # go through order of operations building up a tree
+
+
+# NOTE: Lexer still allows putting 2 operators next to each other; this is fine for brackets 
+# but not valid for regular operations.
 
 lex = MathLexer()
-lex_list = lex.lex_string("-10 + .5. + -5 - 100.7 + 5")
+lex_list = lex.lex_string("10/5*5 + 10*10 - 5") # answer is 125 of course
 print(lex_list)
 
 parser = MathParser()
